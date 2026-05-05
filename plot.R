928a05be14136d503d6e851c55d832e6320b0470
adricv <adria.casacuberta@ub.edu>
2026-05-05 15:58:53 +0200

    cambios en plot
    gráfico nuevo de tendencias

--- plot.R
+++ plot.R
@@ -6,3 +6,9 @@ library(ggplot2)
 
 ggplot(penguins, aes(x = body_mass, y = bill_len, colour = sex)) +
   geom_point() + facet_wrap(~species)
+
+ggplot(penguins, aes(x = body_mass, y = bill_len)) + 
+  geom_smooth(method = lm, aes(colour = species)) + 
+  geom_point()
+
+  
