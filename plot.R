073fb0cd7a57ffc2a5f79d9d20563575fbd2db9c
adricv <adria.casacuberta@ub.edu>
2026-05-05 16:25:05 +0200

    Update plot.R

--- plot.R
+++ plot.R
@@ -11,4 +11,11 @@ ggplot(penguins, aes(x = body_mass, y = bill_len)) +
   geom_smooth(method = lm, aes(colour = species)) + 
   geom_point()
 
+#Graficando otras variables: relación entre isla y peso
+ggplot(penguins, aes(x = island, y = body_mass)) +
+  geom_jitter(width = 0.2, alpha = 0.2, aes(colour = sex)) + 
+  geom_boxplot(fill = "dark grey", alpha = 0.5) +
+  facet_wrap(~species)
+
+
   
